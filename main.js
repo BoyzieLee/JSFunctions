87134c991f424eac00b607f0953f95ed2c8eafd0
Boyzie Mathis <boyzielee@gmail.com>
2015-09-29 17:02:56 -0400

    Normal Assignment Finished

---
+++ main.js
@@ -0,0 +1,19 @@
+
+// Grab My Elements
+var text_one = document.querySelector('#num_one');
+var text_two = document.querySelector('#num_two');
+var answer = document.querySelector('#answer');
+var calculate = document.querySelector('#calculateBtn');
+
+
+// Function to Preform the calculation
+var sum = function () {
+  var num_one = Number(text_one.value);
+  var num_two = Number(text_two.value);
+  var response = num_one + num_two;
+  answer.textContent = response;
+};
+
+
+// Button Click
+calculate.addEventListener('click', sum);
